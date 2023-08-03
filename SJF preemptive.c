887cd6c2614d74682ca13cb30796d94456d1e0b6
Raptor_Halo <119489971+Niharrnaik@users.noreply.github.com>
2023-08-03 22:43:01 +0530

    Update SJF.c

--- SJF preemptive.c
+++ SJF preemptive.c
@@ -9,6 +9,7 @@ int main() {
     
     for (i = 0; i < n; i++) {
         printf("Enter the arrival time for process P%d: ", i + 1);
+
         scanf("%d", &at[i]);
         printf("Enter the burst time for process P%d: ", i + 1);
         scanf("%d", &burst_time[i]);
@@ -38,4 +39,23 @@ int main() {
     
     return 0;
 }
+/*
 
+Enter the number of processes: 4
+Enter the arrival time for process P1: 2
+Enter the burst time for process P1: 1
+Enter the arrival time for process P2: 1
+Enter the burst time for process P2: 5
+Enter the arrival time for process P3: 4
+Enter the burst time for process P3: 1
+Enter the arrival time for process P4: 0
+Enter the burst time for process P4: 6
+P[4]    |       6       |       0
+P[1]    |       5       |       4
+P[3]    |       4       |       3
+P[2]    |       12      |       7
+
+Average waiting time = 3.500000
+Average turnaround time = 6.750000[1] + Done  
+
+*/
